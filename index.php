9537486721db90615ef389b23be87c58a498a796
Janine Fernandez <janinebfernandez@gmail.com>
2017-02-09 13:17:36 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-----Lake Miramar Golf Course -- INDEX.html Page------>
+
+<!---------------------PHP HEAD------------------------->
+<?php include('includes/head.php');?>
+<!------------------PHP HEAD ENDS----------------------->
+
+<!----------------------SECTION 1----------------------->
+<div id="wrapper_index">
+    <p>THIS IS BODY SAMPLE!----------------------------></p>
+</div>
+<!-------------------SECTION 1 ENDS--------------------->
+
+
+<!---------------------PHP FOOT------------------------->
+<?php include('includes/foot.php');?>
+<!-------------------PHP FOOT ENDS---------------------->
