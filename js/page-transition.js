0d28b74014dfab6f4f6550a564b2e181bbaa8b8a
Janine Fernandez <janinebfernandez@gmail.com>
2017-04-30 16:19:01 -0700

    Create page-transition.js

---
+++ js/page-transition.js
@@ -0,0 +1,37 @@
+/*PAGE TRANSITION*/
+    
+    
+
+$(function(){
+         /*
+    Add this code to every page.
+
+    We start by hiding the body, and then fading it in.
+    */
+    $('body').hide().fadeIn(500);
+
+    /*
+    Now we deal with all 'a' tags...
+    */
+    $('.animation-link').click(function(){
+        /*
+        Get the url from this anchors href
+        */
+        var link = $(this).attr('href');
+        /*
+        Fade out the whole page
+        */
+        $('body').fadeOut(50, function(){
+            /*
+            When that's done (on the 'callback') send the browser to the link.
+            */
+            window.location.href = link;
+        });
+        return false;
+    });
+
+});
+
+
+        
+
