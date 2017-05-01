70ed72a469cfda3ce91d2c20a2106df706a2514e
Janine Fernandez <janinebfernandez@gmail.com>
2017-04-30 20:30:29 -0700

    Update includes.js

--- js/includes.js
+++ js/includes.js
@@ -3,15 +3,15 @@
 $(document).ready(function(){
     
 $(function(){
-        $("#headerContent").load("includes/lmgc-header.html");
+        /*$("#headerContent").load("includes/lmgc-header.html");
         $("#footerContent").load("includes/lmgc-footer.html");
-        
+        */
         /*Content loads for PAGES html files*/
         $("#headerContentPages").load("../includes/lmgc-header.html");
         $("#footerContentPages").load("../includes/lmgc-footer.html");
         });
-
-
+}); /*INCLUDES BANNER AND FOOTER ENDS*/
+/*
 $(".teetimes_link").hover(
     function(){
         $(this).addClass('active');
@@ -19,20 +19,9 @@ $(".teetimes_link").hover(
     function() {
         $(this).removeClass('active');
     });
-/*
-$(".teetimes_link").on(mouseover(function(){
-    $(".teetimes_link").css("background-color", "rgb(179, 218, 255)");
-    $(".teetimes_link").css("style.color", "#000000")
-}));
 
-
-$(".teetimes_link").mouseleave(function(){
-    $(".teetimes_link").css("background-color", "rgb(0, 132, 255)");
-});
 */
 
 
 
 
-
-});
