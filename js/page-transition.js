3fc865c10b2af252353447f12056ffbdf3c441a3
Janine Fernandez <janinebfernandez@gmail.com>
2017-04-30 22:56:57 -0700

    Update page-transition.js

--- js/page-transition.js
+++ js/page-transition.js
@@ -1,6 +1,6 @@
 /*PAGE TRANSITION*/
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"> /script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
     
 
 $(function(){
