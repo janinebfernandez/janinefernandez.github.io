3b3d571d73efb773eab149ae08f8393bb6f2d498
Janine Fernandez <janinebfernandez@gmail.com>
2017-05-01 23:37:48 -0700

    Update portfolio-1.js

--- js/portfolio-1.js
+++ js/portfolio-1.js
@@ -1,6 +1,6 @@
 /*JANINE FERNANDEZ portfolio-1.js - VIDEO*/
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    
 
     $(document).ready(function(){
        /* $(function(){
